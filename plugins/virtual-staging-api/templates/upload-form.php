8cb44690a121c1cca8462bf8fffde0396f7a274e
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2024-10-09 20:34:15 +0200

    Add upload form for virtual staging API

---
+++ plugins/virtual-staging-api/templates/upload-form.php
@@ -0,0 +1,4 @@
+<form id="vsai-upload-form" method="post" enctype="multipart/form-data">
+  <input type="file" name="photo" accept="image/*" required>
+  <button type="submit">Upload and Process</button>
+</form>
