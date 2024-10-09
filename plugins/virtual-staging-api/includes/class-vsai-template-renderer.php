459d29a86477c9777ed582951a5059cbd338d803
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2024-10-09 22:35:57 +0200

    Refactor template renderer and asset handling

--- plugins/virtual-staging-api/includes/class-vsai-template-renderer.php
+++ plugins/virtual-staging-api/includes/class-vsai-template-renderer.php
@@ -7,12 +7,14 @@ class VSAI_Template_Renderer
 {
   private $templates = [];
   private $plugin_url;
+  private $used_templates = [];
 
   public function __construct($plugin_url)
   {
     $this->plugin_url = $plugin_url;
     add_shortcode('vsai_template', array($this, 'render_template_shortcode'));
-    add_action('wp_enqueue_scripts', array($this, 'enqueue_template_assets'));
+    add_action('wp_enqueue_scripts', array($this, 'register_template_assets'));
+    add_action('wp_footer', array($this, 'enqueue_template_assets'), 5);
   }
 
   public function register_template($name, $file_path)
@@ -29,6 +31,9 @@ class VSAI_Template_Renderer
     if (!isset($this->templates[$name])) {
       return "Template '$name' not found.";
     }
+
+    // Mark this template as used
+    $this->used_templates[$name] = true;
 
     ob_start();
     extract($data);
@@ -55,14 +60,28 @@ class VSAI_Template_Renderer
     return $this->render_template($atts['name'], $data);
   }
 
+  public function register_template_assets()
+  {
+    // Register (but don't enqueue) CSS and JS
+    wp_register_style('vsai-main-style', $this->plugin_url . 'templates/main/snipped.css');
+    wp_register_style('vsai-upload-style', $this->plugin_url . 'templates/upload/snipped.css');
+
+    wp_register_script('vsai-main-script', $this->plugin_url . 'templates/main/script.js', array('jquery'), null, true);
+    wp_register_script('vsai-upload-script', $this->plugin_url . 'templates/upload/script.js', array('jquery'), null, true);
+  }
+
   public function enqueue_template_assets()
   {
-    // Enqueue main CSS
-    wp_enqueue_style('vsai-main-style', $this->plugin_url . 'templates/main/snipped.css');
-    wp_enqueue_style('vsai-upload-style', $this->plugin_url . 'templates/upload/snipped.css');
+    // Always enqueue CSS for all templates
+    wp_enqueue_style('vsai-main-style');
+    wp_enqueue_style('vsai-upload-style');
 
-    // Enqueue any JavaScript files if needed
-    wp_enqueue_script('vsai-main-script', $this->plugin_url . 'templates/main/script.js', array('jquery'), null, true);
-    wp_enqueue_script('vsai-upload-script', $this->plugin_url . 'templates/upload/script.js', array('jquery'), null, true);
+    // Conditionally enqueue JavaScript based on used templates
+    if (isset($this->used_templates['main'])) {
+      wp_enqueue_script('vsai-main-script');
+    }
+    if (isset($this->used_templates['upload'])) {
+      wp_enqueue_script('vsai-upload-script');
+    }
   }
 }
