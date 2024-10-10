06d368b265050775cb5e30600fbdd3e170520e54
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2024-10-10 14:29:02 +0200

    Add commented out methods to enable WP debugging

--- plugins/virtual-staging-api/virtual-staging-api.php
+++ plugins/virtual-staging-api/virtual-staging-api.php
@@ -10,6 +10,23 @@ Author: Your Name
 if (!defined('ABSPATH'))
   exit;
 
+
+// === DEBUG START ===
+// if (!defined('WP_DEBUG')) {
+//   define('WP_DEBUG', true);
+// }
+// if (!defined('WP_DEBUG_LOG')) {
+//   define('WP_DEBUG_LOG', true);
+// }
+// if (!defined('WP_DEBUG_DISPLAY')) {
+//   define('WP_DEBUG_DISPLAY', false);
+// }
+
+// // Ensure error logging is enabled
+// ini_set('log_errors', 1);
+// ini_set('error_log', '/var/www/html/wp-content/debug.log');
+// === DEBUG END ===
+
 // Define plugin constants
 define('VSAI_PLUGIN_FILE', __FILE__);
 define('VSAI_PLUGIN_DIR', plugin_dir_path(__FILE__));
