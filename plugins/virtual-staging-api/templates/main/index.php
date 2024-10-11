0350d8fbcda6f1bf6924acb25b4dc4e463568b9e
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2024-10-11 17:51:08 +0200

    Refactor script to remove unnecessary vote thumbs code

--- plugins/virtual-staging-api/templates/main/index.php
+++ plugins/virtual-staging-api/templates/main/index.php
@@ -103,24 +103,7 @@
             <div class="md:w-2/3" id="renderPageResultsContainer">
                 <div class="mb-2 flex items-center space-x-4">
                     <h3 class="font-semibold text-gray-800 text-lg md:text-xl mt-0">Results</h3>
-                    <div class="flex items-center space-x-2" data-testid="render-page-vote-thumbs"><svg
-                            stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="transition-colors cursor-pointer hover:text-green-500" height="1em" width="1em"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7 10v12"></path>
-                            <path
-                                d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z">
-                            </path>
-                        </svg><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="transition-colors cursor-pointer hover:text-red-500" height="1em" width="1em"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17 14V2"></path>
-                            <path
-                                d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a3.13 3.13 0 0 1-3-3.88Z">
-                            </path>
-                        </svg></div>
+
                 </div>
                 <div class="relative h-full">
                     <div class="overflow-hidden rounded-xl">
