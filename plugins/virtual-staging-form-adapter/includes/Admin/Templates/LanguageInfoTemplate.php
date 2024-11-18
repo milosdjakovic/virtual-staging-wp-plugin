f1e3b8b411f2c3aec1157e793c68f8e340b5cb24
Milos Djakovic <32454231+milosdjakovic@users.noreply.github.com>
2024-11-18 10:57:14 +0100

    Improve LanguageInfoTemplate layout and clean up HTML structure

--- plugins/virtual-staging-form-adapter/includes/Admin/Templates/LanguageInfoTemplate.php
+++ plugins/virtual-staging-form-adapter/includes/Admin/Templates/LanguageInfoTemplate.php
@@ -10,16 +10,15 @@ class LanguageInfoTemplate
     {
         $current_locale = VSAI_Language_Loader::get_current_locale();
         ?>
-        <div class="notice notice-info">
+        <div class="notice notice-info" style="padding-bottom: 14px;">
             <h2>Language Settings</h2>
             <p>Active language file: <strong><?php echo esc_html($current_locale); ?></strong></p>
-            
+
             <form method="post">
                 <select name="vsai_locale">
                     <?php foreach (glob(plugin_dir_path(VSAI_PLUGIN_FILE) . 'locale/*.json') as $file): ?>
                         <?php $locale = basename($file); ?>
-                        <option value="<?php echo esc_attr($locale); ?>" 
-                            <?php selected($current_locale, $locale); ?>>
+                        <option value="<?php echo esc_attr($locale); ?>" <?php selected($current_locale, $locale); ?>>
                             <?php echo esc_html($locale); ?>
                         </option>
                     <?php endforeach; ?>
@@ -30,7 +29,8 @@ class LanguageInfoTemplate
         <?php
     }
 
-    public function handleRequest() {
+    public function handleRequest()
+    {
         if (isset($_POST['vsai_locale'])) {
             if (false !== get_option(VSAI_Language_Loader::OPTION_KEY)) {
                 update_option(VSAI_Language_Loader::OPTION_KEY, sanitize_text_field($_POST['vsai_locale']));
